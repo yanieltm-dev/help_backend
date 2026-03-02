ef692d0172dffc07cb350ac528a4937caa50a4be
Yaniel Torres Morales <yanieltm99@gmail.com>
2026-03-02 02:46:20 -0500

    chore: update nest-cli.json, jest-e2e.json and metadata for auth

--- src/metadata.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {};
-    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./modules/health/health.controller"), { "HealthController": { "check": { type: Object } } }]] } };
-};
