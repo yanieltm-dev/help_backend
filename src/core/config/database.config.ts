6cd9c44c2cc6bbdb01753a9e17fb811d0ba521d3
Yaniel Torres Morales <yanieltm99@gmail.com>
2026-04-12 00:42:50 -0400

    refactor(database): split DATABASE_URL into individual connection variables

--- src/core/config/database.config.ts
+++ src/core/config/database.config.ts
@@ -8,13 +8,31 @@ export type DatabaseConfig = {
 
 class EnvironmentVariablesValidator {
   @IsString()
-  DATABASE_URL: string;
+  DATABASE_HOST: string;
+
+  @IsString()
+  DATABASE_PORT: string;
+
+  @IsString()
+  DATABASE_NAME: string;
+
+  @IsString()
+  DATABASE_USER: string;
+
+  @IsString()
+  DATABASE_PASSWORD: string;
 }
 
 export default registerAs<DatabaseConfig>('database', () => {
   validateConfig(process.env, EnvironmentVariablesValidator);
 
+  const user = process.env.DATABASE_USER;
+  const password = process.env.DATABASE_PASSWORD;
+  const host = process.env.DATABASE_HOST;
+  const port = process.env.DATABASE_PORT;
+  const name = process.env.DATABASE_NAME;
+
   return {
-    url: process.env.DATABASE_URL as string,
+    url: `postgresql://${user}:${password}@${host}:${port}/${name}`,
   };
 });
